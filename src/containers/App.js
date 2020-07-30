a9b0c69fe28356f3a8dfd74cdfa5cfbc3557a926
Diogenes Fiorezi <diogenesfiorezi@hotmail.com>
2020-07-30 06:16:03 -0300

    Updating de project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import './App.css';
+import React from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "./App.css";
 
 const App = (props) => {
-
- 
-    
-    const filteredRobots = props.robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(props.searchField.toLowerCase());
-    })
-    return props.isPending ?
-      <h1>Loading</h1> :
-      (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={props.onSearchChange}/>
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
-  
-}
+  const filteredRobots = props.robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(props.searchField.toLowerCase());
+  });
+  return props.isPending ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      <SearchBox searchChange={props.onSearchChange} />
+      <Scroll>
+        <CardList robots={filteredRobots} />
+      </Scroll>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-import 'tachyons';
-import {Provider} from "react-redux";
-import {createStore, applyMiddleware, combineReducers} from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import "tachyons";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import {createLogger} from "redux-logger";
-import { searchRobots, requestingRobots } from './redux/reducers';
-import MainComponent from './containers/MainComponent';
+import { createLogger } from "redux-logger";
+import { searchRobots, requestingRobots } from "./redux/reducers";
+import MainComponent from "./containers/MainComponent";
 
 const logger = createLogger();
 const rootReducer = combineReducers({
-    searchRobots, requestingRobots
-})
+  searchRobots,
+  requestingRobots,
+});
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-            <Provider store={store}>
-                <MainComponent />
-            </Provider>, document.getElementById('root'));
-registerServiceWorker();
+  <Provider store={store}>
+    <MainComponent />
+  </Provider>,
+  document.getElementById("root")
+);
